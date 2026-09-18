df4ca3674c2afaa8dec65cfe2d51801ae20e17d8
Cris Symon <crissymonsantos05@gmail.com>
2026-09-18 22:43:54 +0800

    Initial commit

---
+++ app/src/test/java/quarter2/MiniPeta1_Salamanca.java
@@ -0,0 +1,19 @@
+package quarter2;
+import org.junit. Test;
+
+public class MiniPeta1_Salamanca {
+    @Test
+    public void printMyProfileO() {
+        // --- 1. THE INPUT (storing your personal details in variables) ---
+        String myName = "Jennessy";
+        String petName = "Chip";
+        String favFood = "Chicken Breast";
+        int myAge = 17;
+
+        // 2. THE OUTPUT (Printing to the console)
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello,my name is " + myName + "and I am " + myAge + " vears old. ");
+        System.out.println("I have a wonderful pet named" + petName + ".");
+        System.out.println("If I could, I would eat" + favFood + "every single day!");
+    }
+}
